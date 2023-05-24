4a1af4d39fe6eb3d014a3d117cc3870c83355743
Liubomyr <liubomyr.oleksiuk@ucu.edu.ua>
2023-05-24 12:44:39 +0300

    check who win

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -11,6 +11,47 @@ export const Game = ({ className }: GameProps) => {
     const [playerNames, setPlayerNames] = useState(["", "", "", "", "", "", "", "", "", ""]);
     const [timer, setTimer] = useState(60);
     const [isPaused, setIsPaused] = useState(true);
+    const [displayedIndices, setDisplayedIndices] = useState([]);
+    const [mafiaSet, updateMafiaSet]  = useState(new Set());
+    const [townieSet, updateTownieSet]  = useState(new Set());
+
+    function shuffleList(list) {
+        for (let i = list.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [list[i], list[j]] = [list[j], list[i]];
+        }
+        return list;
+    }
+
+    var basedRoles = ["Sheriff", "Don", "Mafia", "Mafia", "Townie", "Townie", 
+                        "Townie", "Townie", "Townie", "Townie", "Townie"];
+    const playerRoles = shuffleList(basedRoles);
+    const [roles, setRoles] = useState(playerRoles);
+
+    const handleButton1Click = (index) => {
+        setDisplayedIndices((prevIndices) => {
+            const updatedIndices = [...prevIndices, index];
+
+            // Check if equal number of townie and mafia players or no mafia remaining
+            const filteredRoles = playerRoles.filter((_, i) => !prevIndices.includes(i));
+            const townieCount = filteredRoles.filter((role) => role === "Townie" || role === "Sheriff").length;
+            const mafiaCount = filteredRoles.filter((role) => role === "Mafia" || role === "Don").length;
+
+            if (townieCount <= mafiaCount) {
+                console.log("Mafia won");
+            } else if (mafiaCount == 0){
+                console.log("Town won");
+            }
+            return updatedIndices;
+        });
+    };
+
+    const handleButton2Click = (name) => {
+        // Handle the logic for the second button click
+        console.log(`Button 2 clicked for ${name}`);
+    };
+
+    const filteredPlayerNames = playerNames.filter((_, index) => !displayedIndices.includes(index));
 
     useEffect(() => {
         let interval = null;
@@ -38,24 +79,21 @@ export const Game = ({ className }: GameProps) => {
         setTimer(60);
         setIsPaused(true);
     };
-
-    function shuffleList(list) {
-        for (let i = list.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [list[i], list[j]] = [list[j], list[i]];
-        }
-        return list;
-    }
-
-    const roles = ["Sheriff", "Don", "Mafia", "Mafia", "Mafia", "Townie", 
-                        "Townie", "Townie", "Townie", "Townie", "Townie"];
-    const playerRoles = shuffleList(roles);
     
     const addName = (name: string, index: number) => {
         console.log(name);
         const updatedPlayerNames = [...playerNames]; // Create a copy of the array
         updatedPlayerNames[index-1] = name; // Modify the copy
         setPlayerNames(updatedPlayerNames); // Update the state variable
+        if (playerRoles[index-1] == "Sheriff" || playerRoles[index-1] == "Townie"){
+            const updatedTownieNames = new Set([...townieSet]);
+            updatedTownieNames.add(name);
+            updateTownieSet(updatedTownieNames);
+        } else {
+            const updatedMafiaNames = new Set([...mafiaSet]);
+            updatedMafiaNames.add(name);
+            updateMafiaSet(updatedMafiaNames);
+        }
         console.log(updatedPlayerNames);
     }
 
@@ -70,11 +108,34 @@ export const Game = ({ className }: GameProps) => {
         return (showForm && <LoginForm key={playerNumber} onClose={handleFormClose} playerNumber={playerNumber} playerRole={playerRole} addName={addName}/>);
     };
 
-    const playerNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    var alivePlayers = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const playerNumbers = [1, 2, 3];
     return (
         <div className={classNames(styles.root, className)}>
             {playerNumbers.map((playerNumber) => RenderLoginForm(playerNumber))}
+
+            <table>
+            <thead>
+                <tr>
+                <th>Name</th>
+                <th>Elliminate</th>
+                <th>Role</th>
+                </tr>
+            </thead>
+            <tbody>
+                {filteredPlayerNames.map((name, index) => (
+                    <tr key={index}>
+                        <td>{name}</td>
+                        <td>
+                        <button onClick={() => handleButton1Click(index)}>Elliminate</button>
+                        </td>
+                        <td>
+                        <button onClick={() => handleButton2Click(name)}>Show Role</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+            </table>
+
             <div className={styles["timer-container"]}>
                 <h1 className={styles["timer-text"]}>Timer: {timer} seconds</h1>
                 <div className={styles["button-container"]}>
